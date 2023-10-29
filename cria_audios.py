3058eb3cab9260d6b3c2453b6848b255e3352cdb
wbrunovieira <wbrunovieira77@gmail.com>
2023-10-29 10:58:14 -0300

    added play to all systems

--- cria_audios.py
+++ cria_audios.py
@@ -1,5 +1,12 @@
 from gtts import gTTS
 
+from subprocess import call    #OSX e Linux
+# from playsound import playsound #windows
+def cria_audio(audio):
+    tts = gTTS(audio, lang='pt-br')
+    tts.save('audios/hello_wanda.mp3')
 
-tts = gTTS(text='Oi Bruninho, a sua Wanda ta aqui', lang='pt-br')
-tts.save('audios/hello_wanda.mp3')
+    call (['afplay', 'audios/hello_wanda.mp3']) # Mac
+# call (['aplay', 'audios/hello_wanda.mp3']) # Linux
+#  playsound('audios/bem_vindo.mp3') # Windows
+cria_audio('Oi Bruninho, a sua Wanda ta aqui')
