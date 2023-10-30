00678d9ed0b435dca26910de016b1e286491008a
wbrunovieira <wbrunovieira77@gmail.com>
2023-10-30 19:24:37 -0300

    added google cloud speech

--- wanda.py
+++ wanda.py
@@ -1,24 +1,25 @@
 import speech_recognition as sr
 
-def monitora_microfone():
+
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "wanda-python-assistent-be1eab582aff.json"
+
+
+def monitora_audio():
     microfone = sr.Recognizer()
     with sr.Microphone() as source:
         print("Aguardando o Comando: ")
         audio = microfone.listen(source)
+        
+            # recognize speech using Google Cloud Speech
+   
     try:
-        print(microfone.recognize_google(audio,  language='pt-BR'))
-                    
+       print("Google Cloud Speech thinks you said " + microfone.recognize_google_cloud(audio, credentials_json="wanda-python-assistent-be1eab582aff.json", language="pt-BR"))
+
     except sr.UnknownValueError:
-                    print("Google not understand audio")
+        print("Google Cloud Speech could not understand audio")
     except sr.RequestError as e:
-                    print("Could not request results from Google Cloud Speech service;".format(e))
-
-monitora_microfone()
-
-
-
-
-
-
-
-
+        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+monitora_audio()
+   
