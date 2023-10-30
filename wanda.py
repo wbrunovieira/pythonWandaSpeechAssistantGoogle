1ebffd11c7996460839cba232cb68368e918f001
wbrunovieira <wbrunovieira77@gmail.com>
2023-10-30 19:42:21 -0300

    added triggler

--- wanda.py
+++ wanda.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 
 
+hotword = "ata"
 
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "wanda-python-assistent-be1eab582aff.json"
@@ -9,17 +10,22 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "wanda-python-assistent-be1eab582
 def monitora_audio():
     microfone = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Aguardando o Comando: ")
-        audio = microfone.listen(source)
-        
-            # recognize speech using Google Cloud Speech
-   
-    try:
-       print("Google Cloud Speech thinks you said " + microfone.recognize_google_cloud(audio, credentials_json="wanda-python-assistent-be1eab582aff.json", language="pt-BR"))
-
-    except sr.UnknownValueError:
-        print("Google Cloud Speech could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+        while True:
+            print("Aguardando o Comando: ")
+            audio = microfone.listen(source)   
+            try:
+                trigger = microfone.recognize_google_cloud(audio, credentials_json="wanda-python-assistent-be1eab582aff.json", language="pt-BR")
+                trigger = trigger.lower()
+                print("Você disse: " + trigger)
+                
+                if hotword in trigger:
+                    print("Comando reconhecido ", trigger)
+                    ###executa os comandos
+                    break
+            except sr.UnknownValueError:
+                print("Google Cloud Speech could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Cloud Speech service; {0}".format(e))
+            
 monitora_audio()
    
